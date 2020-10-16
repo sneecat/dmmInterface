46e231cbcbf475aa9ef24d2f6afcca8122b6fc03
sneecat <snee.andy@gmail.com>
2020-10-16 10:26:12 -0400

    fixed the reconnect function and mode setter

--- main.py
+++ main.py
@@ -2,9 +2,11 @@ import tkinter as tk
 from tkinter import ttk
 import re
 import pyvisa
+# TODO: Make scpithread work with threading lol
 import threading
 from time import sleep
 rm = pyvisa.ResourceManager()
+
 scpiUSBport = 'USB0::0xF4EC::0xEE38::SDM34FBX4R0437::INSTR'
 scpi_TCP_IP = 'TCPIP::192.168.1.120::INSTR'
 
@@ -12,44 +14,60 @@ scpi_TCP_IP = 'TCPIP::192.168.1.120::INSTR'
 class scpiThread():
     ''' wrapper for managing SCPI connection
         '''
+    def connect(self, interval = 3):
+        try:
+            self.instrument = rm.open_resource(scpiUSBport)
+            self.instrument.write_termination = '\r'
+            self.instrument.read_termination = '\n'
+            self.instrument.query_delay = .1
+        
+        except pyvisa.errors.VisaIOError:
+            disconnected = True
+            
+            while disconnected is True:
+                try:
+                    rm.open_resource(self.port).query('*IDN?')
+                except pyvisa.errors.VisaIOError:
+                    
+                    'Cannot connect to instrument. Trying again in %s seconds' % (interval)
+                    sleep(interval)
+                else:
+                    disconnected = False
+                    self.connect()
+                    
+        
     def __init__(self, port, interval=None):
         self.port = port
         self.interval = interval
         self.instrument_mode = 'VOLT'
-        
         self.range = 'DC? 60'
-        try:
-            self.instrument = rm.open_resource(scpiUSBport)
-        except pyvisa.errors.VisaIOError:
-            self.reconnect_loop(3)
-            self.mode_get()
-        else:
-            self.mode_get()
+        self.connect()
+        self.mode_get()
         
-        
-    def reconnect_loop(self, interval=2, timeout=None):
-        ''' reconnect_loop will try to reconnect to the instrument if
-        connection is lost, raise the "disconnected" flag,
-        and eventually timeout if a value is given
-        '''            
-        disconnected = True
-        while disconnected is True:
-            try:
-                rm.open_resource(self.port).query('*IDN?')
-            except pyvisa.errors.VisaIOError:
+    # def reconnect_loop(self, interval=2, timeout=None):
+    #     ''' reconnect_loop will try to reconnect to the instrument if
+    #     connection is lost, raise the "disconnected" flag,
+    #     and eventually timeout if a value is given
+    #     '''            
+    #     disconnected = True
+    #     while disconnected is True:
+    #         try:
+    #             rm.open_resource(self.port).query('*IDN?')
+    #         except pyvisa.errors.VisaIOError:
                 
-                'Cannot connect to instrument. Trying again in %s seconds' % (interval)
-                sleep(interval)
-            else:
-                self.instrument = rm.open_resource(scpiUSBport)
-                disconnected = False
-                return
+    #             'Cannot connect to instrument. Trying again in %s seconds' % (interval)
+    #             sleep(interval)
+    #         else:
+    #             self.instrument = rm.open_resource(scpiUSBport)
+    #             disconnected = False
+    #             return
+    
     def mode_get(self):
         # this ugly POS is how python strips numbers & symbols using regex
 
          try:
             #query instrument for its current mode
-            self.instrument_mode = self.instrument.query('MEAS?')
+            self.instrument_mode = self.instrument.query('CONF?')
             #get rid of unneeded char and whitespace
             self.instrument_mode = self.instrument_mode[1:5].strip()
 
@@ -57,21 +75,28 @@ class scpiThread():
             return self.instrument_mode
            
          except pyvisa.errors.VisaIOError:
-            self.reconnect_loop(3)
+            self.connect()
             
    
     def mode_set(self, mode):
-        self.instrument.write(mode)
-    
+        if mode in ['VOLT', 'RES', 'CURR', 'DIOD', 'CONT']:
+            self.instrument_mode = mode
+        else:
+            print('Not a valid mode. Please use VOLT, RES, CURR, DIOD, or CONT')
+
+    def range_get(self):
+        pass
+    def range_set(self, range):
+        
     def reading(self):
         
         try:
             query = 'MEAS:' + self.instrument_mode + ':' + self.range
             reading = round(float(self.instrument.query(query)), 2)
-            print(str(reading) + ' ' +self.instrument_mode)
+            print(str(reading) + ' ' + self.instrument_mode)
 
         except pyvisa.errors.VisaIOError:
-            self.reconnect_loop()
+            self.connect()
     
     
 
