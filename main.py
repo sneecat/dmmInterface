1225bfc3d82a3603f98cd031bfd5f87299b30b61
sneecat <snee.andy@gmail.com>
2020-10-17 11:45:23 -0400

    clean up methods, dict for range/modes

--- main.py
+++ main.py
@@ -10,12 +10,22 @@ rm = pyvisa.ResourceManager()
 scpiUSBport = 'USB0::0xF4EC::0xEE38::SDM34FBX4R0437::INSTR'
 scpi_TCP_IP = 'TCPIP::192.168.1.120::INSTR'
 
+valid_mode_dict = {
+    'VOLT': ':DC? 60',
+    'CURR': ':DC? 6',
+    'RES': '? AUTO',
+    'DIOD': '?',
+    'CONT': '?',
+    'CAP': '? AUTO'
+}
 
 class scpiThread():
     ''' wrapper for managing SCPI connection
         '''
     def connect(self, interval = 3):
+        
         try:
+
             self.instrument = rm.open_resource(scpiUSBport)
             self.instrument.write_termination = '\r'
             self.instrument.read_termination = '\n'
@@ -27,43 +37,24 @@ class scpiThread():
             while disconnected is True:
                 try:
                     rm.open_resource(self.port).query('*IDN?')
+
                 except pyvisa.errors.VisaIOError:
-                    
                     'Cannot connect to instrument. Trying again in %s seconds' % (interval)
                     sleep(interval)
+
                 else:
                     disconnected = False
                     self.connect()
                     
-        
     def __init__(self, port, interval=None):
         self.port = port
         self.interval = interval
         self.instrument_mode = 'VOLT'
-        self.range = 'DC? 60'
+        self.instrument_range = 'DC? 60'
         self.connect()
         self.mode_get()
-        
-    # def reconnect_loop(self, interval=2, timeout=None):
-    #     ''' reconnect_loop will try to reconnect to the instrument if
-    #     connection is lost, raise the "disconnected" flag,
-    #     and eventually timeout if a value is given
-    #     '''            
-    #     disconnected = True
-    #     while disconnected is True:
-    #         try:
-    #             rm.open_resource(self.port).query('*IDN?')
-    #         except pyvisa.errors.VisaIOError:
-                
-    #             'Cannot connect to instrument. Trying again in %s seconds' % (interval)
-    #             sleep(interval)
-    #         else:
-    #             self.instrument = rm.open_resource(scpiUSBport)
-    #             disconnected = False
-    #             return
     
     def mode_get(self):
-        # this ugly POS is how python strips numbers & symbols using regex
 
          try:
             #query instrument for its current mode
@@ -79,25 +70,26 @@ class scpiThread():
             
    
     def mode_set(self, mode):
-        if mode in ['VOLT', 'RES', 'CURR', 'DIOD', 'CONT']:
+        if mode in valid_mode_dict and valid_mode_dict[mode]:
             self.instrument_mode = mode
+            self.instrument_range = valid_mode_dict.get(mode)
+            self.instrument.write('CONF:' + mode)
         else:
-            print('Not a valid mode. Please use VOLT, RES, CURR, DIOD, or CONT')
+            print('Not a valid mode. Please use VOLT, RES, CURR, DIOD, CAP, or CONT')
 
-    def range_get(self):
-        pass
-    def range_set(self, range):
-        
     def reading(self):
         
         try:
-            query = 'MEAS:' + self.instrument_mode + ':' + self.range
+            query = 'MEAS:' + self.instrument_mode + self.instrument_range
             reading = round(float(self.instrument.query(query)), 2)
-            print(str(reading) + ' ' + self.instrument_mode)
+            if '9.9e+37' in reading:
+                reading = 'O/L'
+            else:
+                print(str(reading) + ' ' + self.instrument_mode)
 
         except pyvisa.errors.VisaIOError:
             self.connect()
-    
+siglent = scpiThread(scpiUSBport, 100) 
     
 
 # class gui():
@@ -118,7 +110,8 @@ class scpiThread():
 #         self.root.geometry('400x200-5+40')
 #         self.root.resizable(FALSE,FALSE)
 #         self.root.attributes("-topmost", 1)
-siglent = scpiThread(scpiUSBport, 100)
+
+siglent.mode_set('CONT')
 def main():
     while 1:
         
